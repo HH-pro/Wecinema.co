2ee69b3e45dbe6f3c3bc3bcb9301d18601ae5d64
HH-pro <hamzamanzoor0011@gmail.com>
2025-09-27 13:18:02 +0500

    Auto update

--- backend/src/controller/listingsController.js
+++ backend/src/controller/listingsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Listing = require("../../models/Listing");
+const Listing = require("../models/listing");
 
 const { ensureOwner, protect } = require("../utils");
 
