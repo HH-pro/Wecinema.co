865a30931eb0e5d6a036c7aff271870ddc110984
HH-pro <hamzamanzoor0011@gmail.com>
2025-09-27 15:31:20 +0500

    Auto update

--- backend/src/controller/listingsController.js
+++ backend/src/controller/listingsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Listing = require("../models/Listing");
+const Listing = require("../../models/Listing");
 const auth = require("../middleware/auth");
 
 /**
