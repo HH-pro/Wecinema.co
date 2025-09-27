12266c16b0b50b92982bc0d6df9508bcee2897a4
HH-pro <hamzamanzoor0011@gmail.com>
2025-09-27 13:08:27 +0500

    Auto update

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const Mongoose = require("mongoose");
-const Listing = require("../../models/Listing");
+const Listing = require("../../Listing");
 
 // ✅ Authenticate Middleware (basic token validation)
 const authenticateMiddleware = (req, res, next) => {
