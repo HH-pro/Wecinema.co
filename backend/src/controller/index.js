e3a28f23f312817d699c0c56984f77581a0a33f3
HH-pro <hamzamanzoor0011@gmail.com>
2025-09-30 12:50:39 +0500

    Auto update

--- backend/src/controller/index.js
+++ backend/src/controller/index.js
@@ -3,17 +3,11 @@ const VideoController = require("./video");
 const domainController = require("./domainController");
 const sentryRouter = require("./sentry");
 
-// ✅ Controllers
-const listingController = require("./listingsController");
-const offerController = require("./offersController");
-const commissionController = require("./commissionsController");
+
 
 module.exports = {
   UserController,
   VideoController,
   domainController,
   sentryRouter,
-  listingController,
-  offerController,
-  commissionController,
 };
