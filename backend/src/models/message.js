4c0892240be77c41714dcb0e1f1a0b1ddfa60d2a
HH-pro <hamzamanzoor0011@gmail.com>
2025-09-30 13:02:26 +0500

    Auto update

--- backend/src/models/message.js
+++ backend/src/models/message.js
@@ -1,23 +1,11 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// const messageSchema = new Schema({
-//   senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-//   receiverId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-//   content: { type: String, required: true },
-//   timestamp: { type: Date, default: Date.now },
-// });
-
-// module.exports = mongoose.model('Message', messageSchema);
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const messageSchema = new mongoose.Schema({
-  orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order', required: true },
-  senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  message: String,
-  attachments: [String],
-  read: { type: Boolean, default: false }
-}, { timestamps: true });
+const messageSchema = new Schema({
+  senderId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  receiverId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
 
 module.exports = mongoose.model('Message', messageSchema);
