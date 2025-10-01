f0a346c63f2609ca0516e75e465bf6ae14c38691
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-01 17:14:30 +0500

    Auto update

--- frontend/src/pages/marketplace/Browse.tsx
+++ frontend/src/pages/marketplace/Browse.tsx
@@ -4,6 +4,7 @@ import CreateListingModal from '../../components/marketplae/CreateListingModal';
 import MarketplaceLayout from '../../components/Layout';
 import { Listing } from '../../types/marketplace';
 import { FiFilter, FiPlus, FiSearch } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 const Browse: React.FC = () => {
   const [listings, setListings] = useState<Listing[]>([]);
