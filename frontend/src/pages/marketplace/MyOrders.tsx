7dff3e344ef3bb4fe9717e8d8de2612702414d11
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-01 14:26:23 +0500

    Auto update

--- frontend/src/pages/marketplace/MyOrders.tsx
+++ frontend/src/pages/marketplace/MyOrders.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import MarketplaceLayout from '../../components/marketplace/MarketplaceLayout';
-import OrderSummary from '../../components/marketplace/OrderSummary';
+import MarketplaceLayout from '../../components/Layout/MarketplaceLayout';
+import OrderSummary from '../../components/marketplae/OrderSummary';
 
 interface Order {
   _id: string;
