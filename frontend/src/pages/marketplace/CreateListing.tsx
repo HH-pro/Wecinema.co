3ba73358413499dcc7f4c2f33efa45a9fa8cb66d
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-02 23:37:41 +0500

    Auto update

--- frontend/src/pages/marketplace/CreateListing.tsx
+++ frontend/src/pages/marketplace/CreateListing.tsx
@@ -63,7 +63,7 @@ const CreateListing: React.FC = () => {
 
     // Try the correct API endpoint
     const response = await axios.post(
-      'http://localhost:3000/api/marketplace/marketplace/listings/create-listing', // Most likely this one
+      'http://localhost:3000/marketplace/listings/create-listing', // Most likely this one
       formDataToSend,
       {
         headers: {
