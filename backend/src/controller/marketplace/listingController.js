3c444780caecbe3889b794ffa55c90e489e64d6e
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-02 22:59:39 +0500

    Auto update

--- backend/src/controller/marketplace/listingController.js
+++ backend/src/controller/marketplace/listingController.js
@@ -29,7 +29,7 @@ router.get("/my-listings", protect, isHypeModeUser, isSeller, async (req, res) =
 // Make sure you're importing the correct model
 
 // Create new listing
-router.post("/create-listing", authenticateMiddleware, async (req, res) => {
+router.post("/create-listing", isSeller, async (req, res) => {
   try {
     console.log('=== CREATE LISTING REQUEST ===');
     console.log('req.user:', req.user);
