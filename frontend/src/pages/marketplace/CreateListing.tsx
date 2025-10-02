8a9a5246b3e9a673610ec9fc99de6c8f0fe61526
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-02 22:52:45 +0500

    Auto update

--- frontend/src/pages/marketplace/CreateListing.tsx
+++ frontend/src/pages/marketplace/CreateListing.tsx
@@ -64,7 +64,7 @@ const token = localStorage.getItem("token") || null;
     formData.mediaFiles.forEach(file => formDataToSend.append('mediaFiles', file));
 
     const response = await axios.post(
-      'http://localhost:3000/marketplace/create-listing',
+      'http://localhost:3000/marketplace/listings/create-listing',
       formDataToSend,
       {
         headers: {
