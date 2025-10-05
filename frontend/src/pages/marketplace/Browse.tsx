774a682c21dddc7d9ae193a5b26e4470e1715bde
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-06 00:28:02 +0500

    Auto update

--- frontend/src/pages/marketplace/Browse.tsx
+++ frontend/src/pages/marketplace/Browse.tsx
@@ -319,7 +319,7 @@ const Browse: React.FC = () => {
               </div>
             </div>
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
               {filteredListings.map(listing => (
                 <ListingCard
                   key={listing._id}
