df841db30a584ca58e4e493aeb5b9e1c3159f10a
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-06 23:17:37 +0500

    Auto update

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -762,3 +762,14 @@ export const simulateWebhook = async (paymentIntentId: string, eventType: string
     eventType
   }, () => {});
 };
+export const makeOffer = async (listingId, offerData) => {
+  const response = await axios.post(`${API_BASE_URL}/offers/make-offer`, {
+    listingId,
+    ...offerData
+  }, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  });
+  return response.data;
+};
