e6deea0be4ad66a5ebc3e507272b6800b7125ed3
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-08 00:04:41 +0500

    Auto update

--- frontend/src/components/marketplae/ListingCard.tsx
+++ frontend/src/components/marketplae/ListingCard.tsx
@@ -49,7 +49,7 @@ const ListingCard: React.FC<ListingCardProps> = ({ listing, onViewDetails, onOff
     }
 
     const response = await axios.post(
-      "http://localhost:3000/marketplace/offers/create-offer",
+      "http://localhost:3000/marketplace/offers/make-offer",
       {
         listingId,
         amount: offerData.amount,
