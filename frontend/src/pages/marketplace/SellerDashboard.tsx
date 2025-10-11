e63bfa25c99bafd46eec06710219429a65193aa8
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-12 00:23:43 +0500

    Auto update

--- frontend/src/pages/marketplace/SellerDashboard.tsx
+++ frontend/src/pages/marketplace/SellerDashboard.tsx
@@ -310,7 +310,7 @@ const SellerDashboard: React.FC = () => {
                 className={`py-2 px-1 border-b-2 font-medium text-sm ${
                   activeTab === 'overview'
                     ? 'border-yellow-600 text-yellow-600'
-                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                    : 'border-transparent text-gray-500 hover:text-gray-100 hover:border-gray-300'
                 }`}
               >
                 Overview
