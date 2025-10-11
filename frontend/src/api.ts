780fc98c211962aecb95aaae56fd63bbe6dee00f
HH-pro <hamzamanzoor0011@gmail.com>
2025-10-12 00:40:58 +0500

    Auto update

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -220,7 +220,7 @@ export const listingAPI = {
     getRequest(`/marketplace/listings/listings/${listingId}`, setLoading),
 
   getMyListings: (setLoading?: React.Dispatch<React.SetStateAction<boolean>>) =>
-    getRequest('/marketplace/listings/my-listings', setLoading),
+    getRequest('/marketplace/listings/user/${userId}/listings', setLoading),
 
   createListing: (formData: FormData, setLoading: React.Dispatch<React.SetStateAction<boolean>>) => {
     return new Promise((resolve, reject) => {
