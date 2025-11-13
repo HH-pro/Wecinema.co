d8b78ec8e6f2eb07fe56bed7d8f77d36ca608b06
HH-pro <hamzamanzoor0011@gmail.com>
2025-11-13 20:58:54 +0500

    Auto update

--- frontend/src/firebase/config.ts
+++ frontend/src/firebase/config.ts
@@ -1,18 +1,22 @@
-// src/firebase/config.ts
+// firebaseConfig.js
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-
+import { getDatabase } from 'firebase/database';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
-
+  apiKey: "AIzaSyDbtw87NpBAema-2eA58hpDuS85dxCBn50",
+  authDomain: "wecinema-21d00.firebaseapp.com",
+  projectId: "wecinema-21d00",
+  storageBucket: "wecinema-21d00.appspot.com",
+  messagingSenderId: "257754899711",
+  appId: "1:257754899711:web:affc6a0e53f9c9806c18ca",
+  measurementId: "G-1TR2CEXH5M"
+  };
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export default app;
+const db = getDatabase(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+googleProvider.addScope('https://www.googleapis.com/auth/user.birthday.read');
+googleProvider.addScope('https://www.googleapis.com/auth/userinfo.email');
+googleProvider.addScope('https://www.googleapis.com/auth/userinfo.profile');
+
+export { auth, googleProvider, app, db };
