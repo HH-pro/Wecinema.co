f00c8cbcf416ad2acde0fd9437f628d88140386e
HH-pro <hamzamanzoor0011@gmail.com>
2025-11-13 15:50:23 +0500

    Auto update

--- frontend/src/components/marketplae/PaymentForm.tsx
+++ frontend/src/components/marketplae/PaymentForm.tsx
@@ -6,7 +6,7 @@ import {
   Elements,
 } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import { useStripePayment } from '../../hooks/useStripePayment';
+import { useStripePayment } from '../../hooks/useStripe';
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY!);
 
