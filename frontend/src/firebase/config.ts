f849eb27a8d4cc18b68f764bee0fa3959de64848
HH-pro <hamzamanzoor0011@gmail.com>
2025-12-08 22:52:57 +0500

    Auto update

---
+++ frontend/src/firebase/config.ts
@@ -0,0 +1,28 @@
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore'; // Add Firestore
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "f08306fa7ba1a4cb5d65693ab63953c1dfe4f458",
+  authDomain: "wecinema-21d00.firebaseapp.com",
+  databaseURL: "https://wecinema-21d00-default-rtdb.firebaseio.com", // ✅ ADD THIS
+  projectId: "wecinema-21d00",
+  storageBucket: "wecinema-21d00.appspot.com",
+  messagingSenderId: "257754899711",
+  appId: "1:257754899711:web:affc6a0e53f9c9806c18ca",
+  measurementId: "G-1TR2CEXH5M"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app); // Realtime Database
+const firestore = getFirestore(app); // Firestore (for chat)
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.addScope('https://www.googleapis.com/auth/user.birthday.read');
+googleProvider.addScope('https://www.googleapis.com/auth/userinfo.email');
+googleProvider.addScope('https://www.googleapis.com/auth/userinfo.profile');
+
+export { auth, googleProvider, app, database, firestore };
