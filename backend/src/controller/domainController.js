dd11ea305259d02cbdf98d5cb071b43fc71e8dd2
HH-pro <hamzamanzoor0011@gmail.com>
2025-12-09 14:33:02 +0500

    Auto update

--- backend/src/controller/domainController.js
+++ backend/src/controller/domainController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Domain = require("../models/domainModel");
 const { sendWhatsAppMessage } = require('../../services/twilioService');
 
 /**
