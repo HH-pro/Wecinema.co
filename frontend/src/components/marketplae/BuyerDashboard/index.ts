4f21aad5c325282c5e2d91299631052df6727f0a
HH-pro <hamzamanzoor0011@gmail.com>
2025-12-15 16:57:23 +0500

    Auto update

--- frontend/src/components/marketplae/BuyerDashboard/index.ts
+++ frontend/src/components/marketplae/BuyerDashboard/index.ts
@@ -4,3 +4,6 @@ export { default as OrderSummary } from './OrderSummary';
 export { default as PaymentDetails } from './PaymentDetails';
 export { default } from './BuyerDashboard';
 
+// Export the new pages
+export { default as MyOffersPage } from '../MyOffers/MyOffersPage';
+export { default as BuyerStatsPage } from '../BuyerStats/BuyerStatsPage';
