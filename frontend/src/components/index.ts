9038da7e943ef947b0bb2f571f943d18fd0ed62a
HH-pro <hamzamanzoor0011@gmail.com>
2025-12-24 16:43:33 +0500

    Auto update

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -13,6 +13,6 @@ export { default as Theme } from "./theme";
 export { default as Search } from "./search";
 
 
-export { default as AnalyticsSection } from "./Analytics/AnalyticsSection";
-export { default as Charts } from "./Analytics/Charts";
-export { default as StatsSummary } from "./Analytics/StatsSummary";
+export { default as AnalyticsSection } from "../components/Analytics/AnalyticsSection";
+export { default as Charts } from "../components/Analytics/Charts";
+export { default as StatsSummary } from "../components/Analytics/StatsSummary";
