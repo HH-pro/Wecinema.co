d89b26783a084784ad1f4d7ab5f7a57f677437f0
HH-pro <hamzamanzoor0011@gmail.com>
2025-12-31 20:43:34 +0500

    Auto update

--- frontend/src/components/marketplae/seller/WithdrawTab.tsx
+++ frontend/src/components/marketplae/seller/WithdrawTab.tsx
@@ -1,6 +1,6 @@
 // src/components/marketplae/seller/WithdrawTab.tsx - FIXED VERSION
 import React, { useState, useEffect } from 'react';
-import { paymentsApi } from '../../../api/marketplace/paymentsApi';
+import { paymentsApi } from '../../../api/paymentsApi';
 
 interface WithdrawTabProps {
   stripeStatus: any;
