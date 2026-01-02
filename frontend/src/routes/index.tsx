b4097a4b214dee93c4b707da5fc8059067c07e73
HH-pro <hamzamanzoor0011@gmail.com>
2026-01-02 22:09:55 +0500

    Auto update

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -46,7 +46,6 @@ import Browse from "../pages/marketplace/Browse";
 import CreateListing from "../pages/marketplace/CreateListing";
 import SellerDashboard from "../pages/marketplace/SellerDashboard";
 import BuyerDashboard from "../components/marketplae/BuyerDashboard/BuyerDashboard";
-import MyOffersPage from '../pages/marketplace/MyOffersPage';
 import MyOrders from "../pages/marketplace/MyOrders";
 import OrderDetails from "../pages/marketplace/OrderDetails";
 import Messages from "../pages/marketplace/Messages";
@@ -93,7 +92,6 @@ const Router: React.FC = () => {
           <Route path="/marketplace/messages/:orderId" element={<Messages />} />
           <Route path="/marketplace/my-orders" element={<MyOrders />} />
           <Route path="/marketplace/orders/:orderId" element={<OrderDetails />} />
-           <Route path="/marketplace/my-offers" element={<MyOffersPage />} />
       <Route path="/marketplace/orders/stats/buyer" element={<BuyerStatsPage />} />
           {/* ========== ADMIN ROUTES ========== */}
           <Route element={<ProtectedRoute />}>
