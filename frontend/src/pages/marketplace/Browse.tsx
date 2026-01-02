10409718f4793050320a686ec348cd3cab3791c9
HH-pro <hamzamanzoor0011@gmail.com>
2026-01-03 00:31:35 +0500

    Auto update

--- frontend/src/pages/marketplace/Browse.tsx
+++ frontend/src/pages/marketplace/Browse.tsx
@@ -762,7 +762,7 @@ const Browse: React.FC = () => {
               </div>
             </div>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
               {filteredListings.map(listing => {
                 const thumbnailUrl = getThumbnailUrl(listing);
                 const videoUrl = getFirstVideoUrl(listing);
