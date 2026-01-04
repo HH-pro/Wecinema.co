775b2f6d0c1498fd2b0f2e65e332b65f447b3789
HH-pro <hamzamanzoor0011@gmail.com>
2026-01-04 19:58:37 +0500

    Auto update

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -11,7 +11,7 @@ import { toast } from "react-toastify";
 const api = axios.create({
   baseURL: import.meta.env.DEV
     ? "http://localhost:3000"
-    : "https://wecinema-main.onrender.com",
+    : "https://wecinema-co.onrender.com/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
