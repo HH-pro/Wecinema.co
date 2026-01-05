865526002135913130b403a7e295e30039b3bd7c
HH-pro <hamzamanzoor0011@gmail.com>
2026-01-05 18:05:54 +0500

    Auto update

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -990,21 +990,23 @@ router.put("/edit/:id", authenticateMiddleware, async (req, res) => {
   }
 });
 
-// Delete user (admin only)
-router.delete("/delete/:id", authenticateMiddleware, isAdmin, async (req, res) => {
-  try {
-    const { id } = req.params;
 
-    const deletedUser = await User.findByIdAndDelete(id);
-    if (!deletedUser) {
-      return res.status(404).json({ error: "User not found" });
-    }
+//delete a particular user - only admin function
+router.delete("/delete/:id",   authenticateMiddleware, isAdmin,async (req, res) => {
+	try {
+		const { id } = req.params;
 
-    res.status(200).json({ message: "User deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting user:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+		// Find the user by ID and delete
+		const deletedUser = await User.findByIdAndDelete(id);
+		if (!deletedUser) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		res.status(200).json({ message: "User deleted successfully" });
+	} catch (error) {
+		console.error("Error deleting user:", error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
 // Get all users
