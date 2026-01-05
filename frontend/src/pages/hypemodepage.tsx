4496ab61fbddf93a48ab741e24af743ed5b1c1fb
HH-pro <hamzamanzoor0011@gmail.com>
2026-01-05 15:04:23 +0500

    Auto update

--- frontend/src/pages/hypemodepage.tsx
+++ frontend/src/pages/hypemodepage.tsx
@@ -26,7 +26,7 @@ const HypeModeProfile = () => {
   // Register user with backend
   const registerUser = async (username: string, email: string, avatar: string, userType: string) => {
     try {
-      const res = await axios.post('https://wecinema.co/api/user/signup', {
+      const res = await axios.post('http://localhost:3000/user/signup', {
         username,
         email,
         avatar,
