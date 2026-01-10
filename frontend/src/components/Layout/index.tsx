e5602073cb6bffe66ff7c1a0daa429d1faf0855c
HH-pro <hamzamanzoor0011@gmail.com>
2026-01-11 01:42:09 +0500

    Auto update

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -350,7 +350,7 @@ const Layout: React.FC<LayoutProps> = ({
   </span>
 </div>
         {/* Common items for both buyer and seller */}
-          {userType === 'seller'|| 'buyer' || (
+          {userType === 'buyer' && (
           <>
         <Link
           to="/marketplace"
