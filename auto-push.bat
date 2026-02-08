f8bf85228b253c214174b01de1e015122bc79d48
HH-pro <hamzamanzoor0011@gmail.com>
2026-02-09 03:34:46 +0500

    Auto update

--- auto-push.bat
+++ auto-push.bat
@@ -1,7 +1,4 @@
 @echo off
-:loop
 git add .
-git diff --cached --quiet || git commit -m "Auto-update %date% %time%"
+git diff --cached --quiet || git commit -m "Manual update %date% %time%"
 git push origin main
-timeout /t 3600
-goto loop
